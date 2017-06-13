b955f948fd5230747641e9485120bd62177bc2b3
hashida <hashida@10.15.0.115>
2017-06-13 15:48:06 +0900

    first commit

---
+++ src/Mixtract.java
@@ -0,0 +1,14 @@
+import net.muse.misc.MuseObject;
+
+public class Mixtract extends MuseObject {
+
+	public Mixtract() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
