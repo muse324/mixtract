d95f66c523e42b1758b0561738d2e81f74028e08
hashida <hashida@192.168.1.8>
2017-06-22 03:13:20 +0900

    move AnalyzeStructureCommand

---
+++ src/net/muse/mixtract/gui/command/AnalyzeStructureCommand.java
@@ -0,0 +1,24 @@
+package net.muse.mixtract.gui.command;
+
+class AnalyzeStructureCommand extends MixtractCommand {
+
+	protected AnalyzeStructureCommand(String... lang) {
+		super(lang);
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see net.muse.misc.Command#execute()
+	 */
+	@Override public void execute() {
+		// _selectedObjects.clearAll();
+		// _target.getGroupArrayList();
+		// _mainFrame.getPianoroll().repaint();
+		// // _mainFrame.getExpressionPanel().clearGroup();
+		// ana = GTTMAnalyzer.run(_target, GTTMAnalyzer.doScoreAnalysis(),
+		// false);
+		// _target.setLatestGroupAnalysis(ana);
+		// _mainFrame.notifyReadTuneData(_target);
+	}
+
+}
--- src/net/muse/mixtract/gui/command/MixtractCommand.java
+++ src/net/muse/mixtract/gui/command/MixtractCommand.java
@@ -233,29 +233,6 @@ public class MixtractCommand extends MuseAppCommand {
 		return super.toString();
 	}
 
-	private static class AnalyzeStructureCommand extends MixtractCommand {
-
-		protected AnalyzeStructureCommand(String... lang) {
-			super(lang);
-		}
-
-		/*
-		 * (非 Javadoc)
-		 * @see net.muse.misc.Command#execute()
-		 */
-		@Override public void execute() {
-			// _selectedObjects.clearAll();
-			// _target.getGroupArrayList();
-			// _mainFrame.getPianoroll().repaint();
-			// // _mainFrame.getExpressionPanel().clearGroup();
-			// ana = GTTMAnalyzer.run(_target, GTTMAnalyzer.doScoreAnalysis(),
-			// false);
-			// _target.setLatestGroupAnalysis(ana);
-			// _mainFrame.notifyReadTuneData(_target);
-		}
-
-	}
-
 	private static final class ApplyPuliseMozartsCommand extends
 			MixtractCommand {
 
