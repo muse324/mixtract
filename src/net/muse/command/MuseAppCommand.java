6d5661d8df54fcd82c2687dd2f29875ebbb94445
hashida <hashida@10.15.0.113>
2018-02-27 00:25:21 +0900

    add MuseAppCommandType; switched

--- src/net/muse/command/MuseAppCommand.java
+++ src/net/muse/command/MuseAppCommand.java
@@ -16,32 +16,32 @@ public class MuseAppCommand extends MuseObject implements Runnable,
 	protected static MIDIController synthe;
 	private static Language _language;
 
-	private static final MuseAppCommand CLOSE = new CloseCommand("Close",
-			"閉じる");
-	private static final MuseAppCommand NULL = new NullCommand("Null");
-	private static final MuseAppCommand PAUSE = new PauseCommand("Pause",
-			"一時停止");
-	private static final MuseAppCommand PLAY = new PlayCommand("Play", "再生");
-	private static final MuseAppCommand QUIT = new QuitCommand("Quit", "終了");
-	private static final MuseAppCommand SAVE = new SaveCommand("Save", "保存");
-	private static final MuseAppCommand SAVEAS = new SaveAsCommand("Save as",
-			"別名で保存");
-	private static final MuseAppCommand SETENV = new SetEnvCommand("Setup",
-			"環境設定");
-	private static final MuseAppCommand SHOW_CONSOLE = new ShowConsoleCommand(
-			"Console", "コンソール");
-	private static final MuseAppCommand STOP = new StopCommand("Stop", "停止");
-	protected static final MuseAppCommand DETAIL = new DetailCommand(
-			"Show parameters", "詳細表示");
-	protected static final MuseAppCommand OPEN_MUSICXML = new OpenMusicXMLCommand(
-			"Open MusicXML...", "MusicXMLを開く...");
-	protected static final MuseAppCommand REFRESH = new RefreshCommand(
-			"Refresh", "更新");
-	protected static final MuseAppCommand RENDER = new RenderCommand("Render",
-			"生成");
-	private static final MuseAppCommand[] commandlist = new MuseAppCommand[] {
-			CLOSE, DETAIL, REFRESH, RENDER, OPEN_MUSICXML, PAUSE, PLAY, QUIT,
-			SAVE, SAVEAS, SETENV, SHOW_CONSOLE, STOP, NULL };
+//	private static final MuseAppCommand CLOSE = new CloseCommand("Close",
+//			"閉じる");
+//	private static final MuseAppCommand NULL = new NullCommand("Null");
+//	private static final MuseAppCommand PAUSE = new PauseCommand("Pause",
+//			"一時停止");
+//	private static final MuseAppCommand PLAY = new PlayCommand("Play", "再生");
+//	private static final MuseAppCommand QUIT = new QuitCommand("Quit", "終了");
+//	private static final MuseAppCommand SAVE = new SaveCommand("Save", "保存");
+//	private static final MuseAppCommand SAVEAS = new SaveAsCommand("Save as",
+//			"別名で保存");
+//	private static final MuseAppCommand SETENV = new SetEnvCommand("Setup",
+//			"環境設定");
+//	private static final MuseAppCommand SHOW_CONSOLE = new ShowConsoleCommand(
+//			"Console", "コンソール");
+//	private static final MuseAppCommand STOP = new StopCommand("Stop", "停止");
+//	protected static final MuseAppCommand DETAIL = new DetailCommand(
+//			"Show parameters", "詳細表示");
+//	protected static final MuseAppCommand OPEN_MUSICXML = new OpenMusicXMLCommand(
+//			"Open MusicXML...", "MusicXMLを開く...");
+//	protected static final MuseAppCommand REFRESH = new RefreshCommand(
+//			"Refresh", "更新");
+//	protected static final MuseAppCommand RENDER = new RenderCommand("Render",
+//			"生成");
+//	private static final MuseAppCommand[] commandlist = new MuseAppCommand[] {
+//			CLOSE, DETAIL, REFRESH, RENDER, OPEN_MUSICXML, PAUSE, PLAY, QUIT,
+//			SAVE, SAVEAS, SETENV, SHOW_CONSOLE, STOP, NULL };
 
 	public static void setLanguage(String val) {
 		_language = Language.create(val);
@@ -71,12 +71,14 @@ public class MuseAppCommand extends MuseObject implements Runnable,
 	 * @return
 	 */
 	protected static MuseAppCommand create(String cmd) {
-		for (MuseAppCommand c : commandlist) {
-			if (cmd.equals(c.name())) {
-				return c;
-			}
-		}
-		return NULL;
+		MuseAppCommandType type = MuseAppCommandType.valueOf(cmd);
+		return type.self();
+//		for (MuseAppCommand c : commandlist) {
+//			if (cmd.equals(c.name())) {
+//				return c;
+//			}
+//		}
+//		return NULL;
 	}
 
 	/**
---
+++ src/net/muse/mixtract/command/MixtractCommandType.java
@@ -0,0 +1,12 @@
+package net.muse.mixtract.command;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public enum MixtractCommandType implements Runnable {
+	;
+
+	@Override public void run() {
+		new NotImplementedException();
+	}
+
+}
