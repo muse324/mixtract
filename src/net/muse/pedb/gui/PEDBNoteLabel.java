9b51954e89a0b138e9adae299a1fe3b150ac2e44
hashida <hashida@172.16.38.156>
2018-08-22 14:19:40 +0900

    PEDBNoteLabel をNoteLabel直下のサブクラスにした

--- src/net/muse/pedb/gui/PEDBNoteLabel.java
+++ src/net/muse/pedb/gui/PEDBNoteLabel.java
@@ -4,7 +4,6 @@ import java.awt.Rectangle;
 
 import net.muse.data.NoteData;
 import net.muse.gui.NoteLabel;
-import net.muse.mixtract.gui.MXNoteLabel;
 import net.muse.pedb.data.PEDBNoteData;
 
 /**
@@ -14,46 +13,22 @@ import net.muse.pedb.data.PEDBNoteData;
  * @author hashida
  *
  */
-public class PEDBNoteLabel extends MXNoteLabel {
+public class PEDBNoteLabel extends NoteLabel {
 
 	public PEDBNoteLabel(NoteData note, Rectangle r) {
 		super(note, r);
-	}
-
-	@Deprecated @Override public NoteLabel child() {
-		return null;
 	}
 
 	@Override public PEDBNoteData getScoreNote() {
 		return (PEDBNoteData) super.getScoreNote();
 	}
 
-	@Deprecated @Override public boolean hasChild() {
-		return false;
-	}
-
-	@Deprecated @Override public boolean hasParent() {
-		return false;
-	}
-
 	@Override public PEDBNoteLabel next() {
 		return (PEDBNoteLabel) super.next();
 	}
 
-	@Deprecated @Override public NoteLabel parent() {
-		return null;
-	}
-
 	@Override public PEDBNoteLabel prev() {
 		return (PEDBNoteLabel) super.prev();
-	}
-
-	@Deprecated @Override public void setChild(NoteLabel child) {
-		super.setChild(child);
-	}
-
-	@Deprecated @Override public void setParent(NoteLabel parent) {
-		super.setParent(parent);
 	}
 
 }
